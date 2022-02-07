5a2bb9544aac8b459ee118f1b4b3fb26facdb5bf
Brandon Johnson <brandon121j@gmail.com>
2022-02-06 22:56:53 -0500

    addition of showTodos function

--- src/components/ShowTodos.js
+++ src/components/ShowTodos.js
@@ -1,22 +1,12 @@
 import React from 'react';
 
 function ShowTodos(todos) {
-
-  const todoShower((todo) => {
-    for (let i = 0; i < todo.length; i++) {
-    return (
-        <div>
-        <h4>{todo}</h4>
-      </div>
-    
-    )
-  }
-  })
-  return (
-  <div>
-
-  </div>
-  );
+	const todoShower = (todo) => {
+		for (let i = 0; i < todo.length; i++) {
+			return <div>{todo[i]}</div>;
+		}
+	};
+	return <div></div>;
 }
 
 export default ShowTodos;
