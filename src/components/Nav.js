cfee2ecfc6eff22feb88626377516d3b5f728b99
Brandon Johnson <brandon121j@gmail.com>
2022-02-08 13:28:30 -0500

    Navbar complete

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { GoThreeBars } from "react-icons/go";
-import '../App.css'
+import { GoThreeBars } from 'react-icons/go';
+import '../App.css';
 
 function Nav() {
 	return (
-    <div className="navbar navbar-expand-lg navbar-light bg-light">
-    <div>
-			<div className='navContainer'>
-				<h1 className="navItem">Todo List</h1>
-        <GoThreeBars size={40} className="navItem"/>
-
+		<div className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className='navContainer'>
+			<div className="navLeft">
+					<h1>Todo List</h1>
 			</div>
-      </div>
+      <div className='navRight'>
+					<GoThreeBars size={40}/>
+        </div>
 		</div>
+    </div>
 	);
 }
 
