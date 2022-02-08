9db227bd6be05041923cece73617306de3e578b4
Brandon Johnson <brandon121j@gmail.com>
2022-02-07 22:19:54 -0500

    Checkpoint Commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { GoThreeBars } from "react-icons/go";
+import '../App.css'
 
 function Nav() {
 	return (
-		<div className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="d-flex justify-content-between">
-				<h1>Todo List</h1>
-				<h1>
-					<i class="bi bi-justify"></i>
-				</h1>
+    <div className="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+			<div className='navContainer'>
+				<h1 className="navItem">Todo List</h1>
+        <GoThreeBars size={40} className="navItem"/>
+
 			</div>
+      </div>
 		</div>
 	);
 }
