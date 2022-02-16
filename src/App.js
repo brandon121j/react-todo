b59a43a2c3dc30badd97dd6d229318fe5d776fa3
Brandon Johnson <brandon121j@gmail.com>
2022-02-15 20:54:38 -0500

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import CreateTodo from './components/CreateTodo';
 import Nav from './components/Nav';
 import ShowTodos from './components/ShowTodos';
 // import Sidebar from './components/Sidebar'
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import './App.css';
 
 
 function App() {
+  
+  const todoReducer = (state, action) => {
+    switch(action.type) {
+      case 'high':
+        return state.push(action);
+      case 'medium': 
+        return state.push(action);
+      case 'low':
+        return state.push(action)
+      default: 
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(todoReducer, []);
+
+  
 
 
   return (
